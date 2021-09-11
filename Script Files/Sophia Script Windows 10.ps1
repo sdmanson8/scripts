05ea2f3fadbd3a73c0a9d9d11e24e1398ccc18f9
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-11 23:59:02 +0200

    Update Sophia Script Windows 10.ps1

--- Script Files/Sophia Script Windows 10.ps1
+++ Script Files/Sophia Script Windows 10.ps1
@@ -1,33 +1,31 @@
 # Download latest Sophia Script release from github
 
-$url = 'https://github.com/farag2/Sophia-Script-for-Windows/releases/latest'
-$request = [System.Net.WebRequest]::Create($url)
-$response = $request.GetResponse()
-$realTagUrl = $response.ResponseUri.OriginalString
-$version = $realTagUrl.split('/')[-1].Trim('v')
-$fileName = "Sophia.Script.v$version.zip"
-$realDownloadUrl = $realTagUrl.Replace('tag', 'download') + '/' + $fileName
+$url = 'https://github.com/farag2/Sophia-Script-for-Windows/archive/refs/heads/master.zip'
+$fileName = "Sophia Script Master.zip"
 
 Write-Host Downloading Sophia Script
-#Write-Host Downloading Sophia Script v$version
-Invoke-WebRequest -Uri $realDownloadUrl -OutFile $env:TEMP/$fileName
+#Write-Host Downloading Sophia Script
+Invoke-WebRequest -Uri $url -OutFile $env:TEMP/$fileName
 
 # temp dir
 $TempDir = [System.IO.Path]::GetTempPath()
 
 PAUSE
 Write-Host Extracting release files
-Expand-Archive -path "$TempDir\$fileName" -DestinationPath "C:\Sophia Script v$version"
+Expand-Archive -path "$TempDir\$fileName" -DestinationPath "C:\Sophia Script Master"
 Remove-Item $TempDir\$fileName
 
-#Open Sophia Script File
-PAUSE
-& "C:\Program Files\Notepad++\notepad++.exe" "C:\Sophia Script v$version\Sophia Script v$version\Sophia.ps1"
+#Open Sophia Script File (Powershell)
+#PAUSE
+#& "C:\Program Files\Notepad++\notepad++.exe" "C:\Sophia Script Master\Sophia-Script-for-Windows-master\Sophia\PowerShell 7\Sophia.ps1"
 
 #Write-Host Running Sophia Script
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-& 'C:\Sophia Script*\Sophia Script*\Sophia.ps1'
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+#& 'C:\Sophia Script Master\Sophia-Script-for-Windows-master\Sophia\PowerShell 7\Sophia.ps1'
+
+#Open Wrapper (GUI)
+& "C:\Sophia Script Master\Sophia-Script-for-Windows-master\Wrapper\SophiaScriptWrapper.exe"
 
 PAUSE
 Write-Host Removing Sophia Script Folder
-Remove-Item 'C:\Sophia Script*'
+Remove-Item 'C:\Sophia Script Master'
