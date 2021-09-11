6e7f4b992b96adb4afa6ddf7b8ea0656a0f309ce
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-11 19:18:10 +0200

    Add files via upload

---
+++ Script Files/Uninstall Edge.ps1
@@ -0,0 +1,22 @@
+# Uninstall Edge
+
+Write-Host Downloading Edge Legacy Uninstaller
+Invoke-WebRequest -Uri https://gorazy.com/it-support/downloads/uninstall_edge.zip -OutFile C:\uninstall_edge.zip
+
+PAUSE
+Write-Host Extracting release files
+Expand-Archive "C:\uninstall_edge.zip" -DestinationPath "C:\uninstall_edge"
+Remove-Item C:\uninstall_edge.zip
+
+PAUSE
+Write-Host Run Edge Legacy Uninstaller
+& "C:\uninstall_edge\Uninstall Edge.cmd"
+
+PAUSE
+Removing Edge Legacy Uninstaller folders
+Remove-Item "C:\uninstall_edge"
+
+PAUSE
+Write-Host Uninstalling Edge Latest
+cd %PROGRAMFILES(X86)%\Microsoft\Edge\Application\8*\Installer
+setup --uninstall --force-uninstall --system-level
