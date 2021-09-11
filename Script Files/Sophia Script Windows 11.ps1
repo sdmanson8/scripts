4a07bff1dece56eece386c2607426edea58a85d2
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 00:13:51 +0200

    Update Sophia Script Windows 11.ps1

--- Script Files/Sophia Script Windows 11.ps1
+++ Script Files/Sophia Script Windows 11.ps1
@@ -23,6 +23,7 @@ Remove-Item $TempDir\$fileName
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 #& 'C:\Sophia Script Master\Sophia-Script-for-Windows-master\Sophia\Windows 11\Sophia.ps1'
 
+PAUSE
 #Open Wrapper (GUI)
 & "C:\Sophia Script Master\Sophia-Script-for-Windows-master\Wrapper\SophiaScriptWrapper.exe"
 
