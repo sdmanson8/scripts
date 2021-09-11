54f35da40625b67077561aa058072829f9823b8f
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-11 19:43:44 +0200

    Update Uninstall Edge.ps1

--- Script Files/Uninstall Edge.ps1
+++ Script Files/Uninstall Edge.ps1
@@ -1,21 +1,21 @@
 # Uninstall Edge
 
-#Write-Host Downloading Edge Legacy Uninstaller
-#Invoke-WebRequest -Uri https://gorazy.com/it-support/downloads/uninstall_edge.zip -OutFile C:\uninstall_edge.zip
+Write-Host Downloading Edge Legacy Uninstaller
+Invoke-WebRequest -Uri https://gorazy.com/it-support/downloads/uninstall_edge.zip -OutFile C:\uninstall_edge.zip
 
-#PAUSE
-#Write-Host Extracting release files
-#Expand-Archive "C:\uninstall_edge.zip" -DestinationPath "C:\uninstall_edge"
-#Remove-Item C:\uninstall_edge.zip
+PAUSE
+Write-Host Extracting release files
+Expand-Archive "C:\uninstall_edge.zip" -DestinationPath "C:\uninstall_edge"
+Remove-Item C:\uninstall_edge.zip
 
-#PAUSE
-#Write-Host Run Edge Legacy Uninstaller
-#& "C:\uninstall_edge\Uninstall Edge.cmd"
+PAUSE
+Write-Host Run Edge Legacy Uninstaller
+& "C:\uninstall_edge\Uninstall Edge.cmd"
 
-#PAUSE
-#Removing Edge Legacy Uninstaller folders
-#Remove-Item "C:\uninstall_edge"
+PAUSE
+Removing Edge Legacy Uninstaller folders
+Remove-Item "C:\uninstall_edge"
 
-#PAUSE
+PAUSE
 Write-Host Uninstalling Edge Latest
 & "C:\Program Files (x86)\Microsoft\Edge\Application\9*\Installer\setup.exe" --uninstall --system-level --verbose-logging --force-uninstall
