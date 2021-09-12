dd036ae91e4ad1249b1f38aae46d6972279ee277
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 11:34:08 +0200

    Update Windows First Run Script.ps1

--- Script Files/Windows First Run Script.ps1
+++ Script Files/Windows First Run Script.ps1
@@ -129,5 +129,6 @@
     Write-Output "exit"
     $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script.ps1
     Invoke-Expression $($ScriptFromGithHub.Content)
- } 
+   } 
+ }
  mainmenu  
