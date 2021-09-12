baf4f30e04e8aaa57632f18d3baf61ff277d9b89
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 11:32:49 +0200

    Update Windows First Run Script.ps1

--- Script Files/Windows First Run Script.ps1
+++ Script Files/Windows First Run Script.ps1
@@ -130,5 +130,4 @@
     $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script.ps1
     Invoke-Expression $($ScriptFromGithHub.Content)
  } 
-      }  
  mainmenu  
