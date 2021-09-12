7881a3100d20d1fb6a749883215158af032ddffd
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 22:59:51 +0200

    Update Remove-Windows10-Bloat.bat

--- Script Files/Remove-Windows10-Bloat.bat
+++ Script Files/Remove-Windows10-Bloat.bat
@@ -97,6 +97,7 @@ PowerShell -Command "Get-AppxPackage *zunevideo* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *WindowsSoundRecorder* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *bingnews* | Remove-AppxPackage"
+::REM PowerShell -Command "Get-AppxPackage *WindowsCalculator* | Remove-AppxPackage"
 REM PowerShell -Command "Get-AppxPackage *WindowsMaps* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *Sway* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *CommsPhone* | Remove-AppxPackage"
@@ -109,3 +110,29 @@ PowerShell -Command "Get-AppxPackage *bingweather* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *xboxapp* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *Drawboard PDF* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *solitairecollection* | Remove-AppxPackage"
+
+
+::@rem NOW JUST SOME TWEAKS
+::REM *** Show hidden files in Explorer ***
+::REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
+
+::REM *** Show super hidden system files in Explorer ***
+::REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d 1 /f
+
+::REM *** Show file extensions in Explorer ***
+::reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t  REG_DWORD /d 0 /f
+
+
+
+::REM *** Uninstall OneDrive ***
+::start /wait "" "%SYSTEMROOT%\SYSWOW64\ONEDRIVESETUP.EXE" /UNINSTALL
+::rd C:\OneDriveTemp /Q /S >NUL 2>&1
+::rd "%USERPROFILE%\OneDrive" /Q /S >NUL 2>&1
+::rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S >NUL 2>&1
+::rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S >NUL 2>&1
+::reg add "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+::reg add "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\ShellFolder" /f /v Attributes /t REG_DWORD /d 0 >NUL 2>&1
+::echo OneDrive has been removed. Windows Explorer needs to be restarted.
+pause
+start /wait TASKKILL /F /IM explorer.exe
+start explorer.exe
