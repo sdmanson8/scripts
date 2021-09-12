ecbbe0eb5c60e2826a3af6bdb1ccae0f87f2a7c8
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 16:06:42 +0200

    Add files via upload

---
+++ Script Files/Setup and Configure for Domain Policies.ps1
@@ -0,0 +1,48 @@
+﻿ #Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
+ function areyousure {$areyousure = read-host "Are you sure you want to exit? (y/n)"  
+           if ($areyousure -eq "y"){exit}  
+           if ($areyousure -eq "n"){mainmenu}  
+                                } 
+  #Mainmenu function. Contains the screen output for the menu and waits for and handles user input.  
+ function mainmenu{  
+ clear  
+ echo "---------------------------------------------------------"  
+ echo "        Setup and Configure for Domain Policies"
+ echo ""
+ echo ""  
+ echo "    1. Configure/Connect to Domain VPN"
+ echo "    2. Join Domain Network"
+ echo "    3. Domain GPUpdate"
+ echo "    . exit" 
+ echo "" 
+ echo ""
+ echo "---------------------------------------------------------"  
+ $answer = read-host "Please Make a Selection"  
+ if ($answer -eq 1){
+    clear
+    # Configure/Connect to Domain VPN
+    Write-Output "Configure/Connect to Domain VPN"
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/vpn.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ }  
+ if ($answer -eq 2){
+    clear
+    # Join Domain Network
+    Write-Output "Join Domain Network"
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/join%20domain.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ }  
+ if ($answer -eq 2){
+    clear
+    # Domain GPUpdate
+    Write-Output "Domain GPUpdate"
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/gpupdate.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ }
+ if ($answer -eq 3){areyousure} 
+ else {write-host -ForegroundColor red "Invalid Selection"  
+       sleep 5  
+       mainmenu  
+      }  
+                }  
+ mainmenu  
