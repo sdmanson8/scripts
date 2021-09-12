89417697dfeef7778f70ba948934d8720fa2b87c
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-12 15:10:30 +0200

    Add files via upload

---
+++ Script Files/join domain.ps1
@@ -0,0 +1,25 @@
+﻿while ($true)
+        {
+            $vpnname = "Reflex VPN"
+            $address = "vpn.reflex.co.za"
+            $vpnusername = "sheldonm"
+            $vpnpassword = "ccrse3a6ti"
+
+$msg     = 'Do you want to check if Domain VPN is connected? [Type Y/N]'
+do {
+            $response = Read-Host -Prompt $msg
+            if ($response -eq 'y') {
+            Write-Host Connect to VPN configuration
+            $vpn = Get-VpnConnection | where {$_.Name -eq "Reflex VPN"}
+            if ($vpn.ConnectionStatus -eq "Disconnected")
+            {
+                $cmd = $env:WINDIR + "\System32\rasdial.exe"
+                $expression = "$cmd ""$vpnname"" sheldonm ccrse3a6ti"
+                Invoke-Expression -Command $expression 
+            }
+            PAUSE
+            Write-Host Join PC to Domain
+            add-computer –domainname reflex.co.za -Credential Reflex\SheldonM -restart –force
+      }
+} until ($response -eq 'n')
+}
---
+++ Script Files/vpn.ps1
@@ -0,0 +1,24 @@
+﻿while ($true)
+        {
+            $vpnname = "Reflex VPN"
+            $address = "vpn.reflex.co.za"
+            $vpnusername = "sheldonm"
+            $vpnpassword = "ccrse3a6ti"
+$msg     = 'Do you create a VPN Profile? [Type Y/N]'
+do {
+            $response = Read-Host -Prompt $msg
+            if ($response -eq 'y') {
+            Write-Host Add VPN configuration
+            Add-VpnConnection -Name "$vpnname" -ServerAddress "$address" -TunnelType PPTP -EncryptionLevel Required -RememberCredential -PassThru
+        }
+} until ($response -eq 'n')
+PAUSE
+            Write-Host Connect to VPN configuration
+            $vpn = Get-VpnConnection | where {$_.Name -eq "Reflex VPN"}
+            if ($vpn.ConnectionStatus -eq "Disconnected")
+            {
+                $cmd = $env:WINDIR + "\System32\rasdial.exe"
+                $expression = "$cmd ""$vpnname"" sheldonm ccrse3a6ti"
+                Invoke-Expression -Command $expression 
+            }
+        }
