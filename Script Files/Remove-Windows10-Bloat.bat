fd868f70ef4928b7d2a78133e16f0b88cc86ae95
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-13 09:18:03 +0200

    Update Remove-Windows10-Bloat.bat

--- Script Files/Remove-Windows10-Bloat.bat
+++ Script Files/Remove-Windows10-Bloat.bat
@@ -1,7 +1,7 @@
 ::Create Restore Point
 Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Windows Bloatware Removal", 100, 12
 
-@rem *** Disable Some Service ***
+::@rem *** Disable Some Service ***
 sc stop DiagTrack
 sc stop diagnosticshub.standardcollector.service
 sc stop dmwappushservice
@@ -46,7 +46,7 @@ REM schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporti
 REM schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /Disable
 
 
-@rem *** Remove Telemetry & Data Collection ***
+::@rem *** Remove Telemetry and Data Collection ***
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Metadata" /v PreventDeviceMetadataFromNetwork /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\MRT" /v DontOfferThroughWUAU /t REG_DWORD /d 1 /f
@@ -57,36 +57,36 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelem
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\AutoLogger-Diagtrack-Listener" /v "Start" /t REG_DWORD /d 0 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\SQMLogger" /v "Start" /t REG_DWORD /d 0 /f
 
-@REM Settings -> Privacy -> General -> Let apps use my advertising ID...
+::@REM Settings -> Privacy -> General -> Let apps use my advertising ID...
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v Enabled /t REG_DWORD /d 0 /f
 REM - SmartScreen Filter for Store Apps: Disable
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" /v EnableWebContentEvaluation /t REG_DWORD /d 0 /f
 REM - Let websites provide locally...
 reg add "HKCU\Control Panel\International\User Profile" /v HttpAcceptLanguageOptOut /t REG_DWORD /d 1 /f
 
-@REM WiFi Sense: HotSpot Sharing: Disable
+::@REM WiFi Sense: HotSpot Sharing: Disable
 reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" /v value /t REG_DWORD /d 0 /f
-@REM WiFi Sense: Shared HotSpot Auto-Connect: Disable
+::@REM WiFi Sense: Shared HotSpot Auto-Connect: Disable
 reg add "HKLM\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" /v value /t REG_DWORD /d 0 /f
 
-@REM Change Windows Updates to "Notify to schedule restart"
+::@REM Change Windows Updates to "Notify to schedule restart"
 reg add "HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v UxOption /t REG_DWORD /d 1 /f
-@REM Disable P2P Update downlods outside of local network
+::@REM Disable P2P Update downlods outside of local network
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v DODownloadMode /t REG_DWORD /d 0 /f
 
 
-REM *** Hide the search box from taskbar. You can still search by pressing the Win key and start typing what you're looking for ***
-REM 0 = hide completely, 1 = show only icon, 2 = show long search box
+::@REM *** Hide the search box from taskbar. You can still search by pressing the Win key and start typing what youre looking for ***
+::REM 0 = hide completely, 1 = show only icon, 2 = show long search box
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f
 
-REM *** Disable MRU lists (jump lists) of XAML apps in Start Menu ***
+::REM *** Disable MRU lists (jump lists) of XAML apps in Start Menu ***
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackDocs" /t REG_DWORD /d 0 /f
 
-REM *** Set Windows Explorer to start on This PC instead of Quick Access ***
-REM 1 = This PC, 2 = Quick access
+::REM *** Set Windows Explorer to start on This PC instead of Quick Access ***
+::REM 1 = This PC, 2 = Quick access
 REM reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
 
-@rem Remove Apps
+::@rem Remove Apps
 PowerShell -Command "Get-AppxPackage *3DBuilder* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *Getstarted* | Remove-AppxPackage"
 PowerShell -Command "Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage"
@@ -135,4 +135,4 @@ PowerShell -Command "Get-AppxPackage *solitairecollection* | Remove-AppxPackage"
 ::echo OneDrive has been removed. Windows Explorer needs to be restarted.
 pause
 start /wait TASKKILL /F /IM explorer.exe
-start explorer.exe
+shutdown /r now
