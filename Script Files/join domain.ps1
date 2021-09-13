34c588e182c4eeb8945e090d4878b7587b8b9df0
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-13 23:12:32 +0200

    Update join domain.ps1

--- Script Files/join domain.ps1
+++ Script Files/join domain.ps1
@@ -1,2 +1,2 @@
             Write-Host Join PC to Domain
-            add-computer –domainname reflex.co.za -Credential Reflex\SheldonM -restart –force
+            add-computer –domainname "example.com" -Credential example\username -restart –force
