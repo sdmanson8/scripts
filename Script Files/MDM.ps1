fa6d353632edc9bfbd9749e983a7f154cf401cf0
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-13 07:37:28 +0200

    Update MDM.ps1

--- Script Files/MDM.ps1
+++ Script Files/MDM.ps1
@@ -1,22 +1,3 @@
-            $vpnname = "Reflex VPN"
-            $address = "vpn.reflex.co.za"
-            $vpnusername = "sheldonm"
-            $vpnpassword = "ccrse3a6ti"
-
-$msg     = 'Do you want to check if Domain VPN is connected? [Type Y/N]'
-do {
-            $response = Read-Host -Prompt $msg
-            if ($response -eq 'y') {
-            Write-Host Connect to VPN configuration
-            $vpn = Get-VpnConnection | where {$_.Name -eq "Reflex VPN"}
-            }
-            if ($vpn.ConnectionStatus -eq "Disconnected")
-            {
-                $cmd = $env:WINDIR + "\System32\rasdial.exe"
-                $expression = "$cmd ""$vpnname"" sheldonm ccrse3a6ti"
-                Invoke-Expression -Command $expression 
-            }
-} until ($response -eq 'n')
 Write-Host Adding Domain User to Local Admin group
 Add-LocalGroupMember -Group "Administrators" -Member "reflex\sheldonm"
 
