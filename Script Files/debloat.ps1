ba7d6612e6bce4c4b54cd3f993648b3fbdcc4da0
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-13 08:58:02 +0200

    Update debloat.ps1

--- Script Files/debloat.ps1
+++ Script Files/debloat.ps1
@@ -1,1 +1,3 @@
+#Create Restore Point
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -NoExit -Command "Checkpoint-Computer -Description "Windows Tweak/Optimizer Tool" -RestorePointType "MODIFY_SETTINGS"; " ' " -Verb RunAs}"
 iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JJ8R4'))
