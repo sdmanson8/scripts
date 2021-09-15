4ed1bcf7b64b0bdc0fb5b0ea3849dd11f7fca873
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-15 07:44:08 +0200

    Add files via upload

---
+++ Script Files/DomainApps.ps1
@@ -0,0 +1,39 @@
+    # Avaya Agent Desktop
+    Write-Host Opening Webpage to Download Prerequisites
+    Start-Process http://avaya-accs/agentdesktop/setup.exe
+    PAUSE
+    Write-Host "Installing Avaya Agent Prerequisites"
+    Start-Process C:\Users\sheldonm\Downloads\setup.exe
+    PAUSE
+    Remove-Item C:\Users\sheldonm\Downloads\setup.exe
+    Write-Host Opening Webpage to Download Avaya Agent Desktop
+    Start-Process http://avaya-accs/agentdesktop/CCADClickOnce.application
+
+    # Office 365 (Unlicensed)
+    Write-Host "Opening Webpage to Download Office 365 (Unlicensed)"
+    Start-Process "https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365ProPlusRetail&platform=X64&language=en-us&TaxRegion=db&correlationId=738b3e5c-6a37-4a2b-8f20-3cdd08477dd8&token=0d16b52a-7f7c-4147-8e28-50e755b1eb69&version=O16GA&source=O15OLSO365&Br=2"
+    PAUSE
+    Write-Host "Installing Office 365"
+    Start-Process C:\Users\sheldonm\Downloads\OfficeSetup.exe
+    PAUSE
+    Remove-Item C:\Users\sheldonm\Downloads\OfficeSetup.exe
+
+    # Install Avaya Workplace
+    Write-Output "Installing Avaya Workplace"
+    msiexec.exe /i '\\zarbkfs01\Company Folder\Avaya IX Workplace Setup 3.8.0.136.14.msi' 
+
+    # Install Ninja
+    Write-Output "Installing Ninja"
+    msiexec.exe /i '\\zarbkfs01\Company Folder\BU - EUC\BU - Managed Services\#Software\#NINJA_INSTALLS\REFLEX\reflexsolutionsworkstationmainoffice-4.4.6012-windows-installer.msi'
+
+    # Install ESET
+    Write-Output "Installing ESET"
+    Start-Process -Wait -FilePath '\\zarbkfs01\Company Folder\BU - EUC\BU - Managed Services\#Software\ESET\AIO_FOR_ALL_CLIENTS\_WORK_STATION_AIO_ALL_CLIENTS_x64_en_US.exe' -ArgumentList '/S' -PassThru
+
+    # Install FortiClient VPN
+    Write-Output "Installing FortiClient VPN"
+    Start-Process -Wait -FilePath '\\zarbkfs01\Company Folder\BU - EUC\BU - Managed Services\#Software\Fortinet\FortiClientSetup_6.0.9.0277_x64.exe' -ArgumentList '/S' -PassThru
+
+    # Install Reflex Remote Support
+    Write-Output "Installing Reflex Remote Support"
+    msiexec.exe /i '\\zarbkfs01\Company Folder\BU - EUC\BU - Managed Services\#Software\RS\Reflex Internal\Reflex_RS_PCs.msi'
