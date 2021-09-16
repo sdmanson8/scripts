ec3d5381a6ca069c6e8a0121a2cfcc1321c0913a
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-16 16:05:54 +0200

    Update Uninstall Edge.ps1

--- Script Files/Uninstall Edge.ps1
+++ Script Files/Uninstall Edge.ps1
@@ -17,32 +17,6 @@ Write-Host Removing Edge Legacy Uninstaller folders
 Remove-Item "C:\uninstall_edge"
 
 Clear-Host
-$msg     = 'Do you want to rather uninstall Microsoft Chromium Edge using Geek Uninstaller? [Type Y/N]'
-do {
-    $response = Read-Host -Prompt $msg
-    if ($response -eq 'y') {
-        # prompt to run (Beginner) Remove Windows Bloatware by ChrisTitusTech
-    
-    PAUSE
-    Write-Host Downloading Geek Uninstaller
-    Invoke-WebRequest -Uri https://geekuninstaller.com/geek.zip -OutFile C:\geek.zip
-    
-    PAUSE
-    Write-Host Extracting files
-    Expand-Archive "C:\geek.zip" -DestinationPath "C:\Geek Uninstaller"
-    Remove-Item C:\geek.zip
-    
-    PAUSE
-    Write-Host Running Geek Uninstaller PS!! RIGHT CLICK ON ALL MICROSOFT EDGE INSTANCES AND SELECT "FORCE REMOVAL"
-    & "C:\Geek Uninstaller\geek.exe"
-
-    PAUSE
-    Write-Host Removing Edge Legacy Uninstaller folders
-    Remove-Item "C:\Geek Uninstaller"
-    }
-} until ($response -eq 'n')
-
-Clear-Host
 $sysapppath = "$env:systemroot\SystemApps"
 $sysapps = @(
     "Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
@@ -72,3 +46,22 @@ foreach ($sysapp in $sysapps) {
         }
     }
 }
+
+Clear-Host
+    Write-Host Downloading Geek Uninstaller
+    Invoke-WebRequest -Uri https://geekuninstaller.com/geek.zip -OutFile C:\geek.zip
+    
+    PAUSE
+    Write-Host Extracting files
+    Expand-Archive "C:\geek.zip" -DestinationPath "C:\Geek Uninstaller"
+    Remove-Item C:\geek.zip
+    
+    PAUSE
+    Write-Host Running Geek Uninstaller PS!! RIGHT CLICK ON ALL MICROSOFT EDGE INSTANCES AND SELECT "FORCE REMOVAL"
+    & "C:\Geek Uninstaller\geek.exe"
+
+    PAUSE
+    Write-Host Removing Edge Legacy Uninstaller folders
+    Remove-Item "C:\Geek Uninstaller"
+
+
