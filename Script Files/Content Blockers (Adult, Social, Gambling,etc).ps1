1a7307b5fb82683f055d42ceda19e8ac21414d99
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-16 14:43:35 +0200

    Update Content Blockers (Adult, Social, Gambling,etc).ps1

--- Script Files/Content Blockers (Adult, Social, Gambling,etc).ps1
+++ Script Files/Content Blockers (Adult, Social, Gambling,etc).ps1
@@ -83,83 +83,23 @@ $appdata = Get-Childitem env:APPDATA | %{ $_.Value }
     Write-Output "Adding Safe Search Value to Hosts File"
     Add-Content C:\Windows\System32\drivers\etc\hosts "216.239.38.120 www.google.com         #forcesafesearch"
  }
-  if ($answer -eq 6){
-    Clear-Host
- function mainmenu{  
- Clear-Host  
- echo "---------------------------------------------------------"  
- echo "     Modify Browser Settings (Filtering)"
- echo ""
- echo ""   
- echo "    1. Modify Chrome Settings"
- echo "    2. Modify Microsoft Edge Settings" 
- echo ""
- echo "" 
- echo ""
- echo "---------------------------------------------------------"  
- $answer = read-host "Please Make a Selection"  
-
-  if ($answer -eq 1){
-    Clear-Host
-    # Modify Chrome Settings
-    Write-Host Is chrome Installed?
-    $w64=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | where-Object DisplayName -like 'google chrome*'
-    $w32=Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*  | where-Object DisplayName -like 'google chrome*'
-    if ($w64 -or $w32)
-    {
-    Write-output "Google Chrome is already installed on your machine."
-    }
-    Else{
-    Write-Output "Google Chrome is not installed on your machine."
-    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Chrome.ps1
-    Invoke-Expression $($ScriptFromGithHub.Content)
-    } 
-    # Check if Chrome is Running, Stop Chrome if Running
-    Write-Host "Is Chrome Running?"
-    if((get-process "chrome" -ea SilentlyContinue) -eq $Null){ 
-        echo "Not Running" 
-    }
-    else{ 
-    echo "Running, Stopping Process"
-    Stop-Process -processname "chrome"
-        }
-    # Tweak Chrome
-    Write-Host "Disabling Guest Mode"
-    REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserGuestModeEnabled /t REG_DWORD /d 0
-    #Write-Host "Disabling Add Profile"
-    #REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserAddPersonEnabled /t REG_DWORD /d 0
-    Write-Host "Disabling Incognito"
-    REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v IncognitoModeAvailability /t REG_DWORD /d 1
-}
-  if ($answer -eq 2){
-    Clear-Host
-    # Check if Microsoft Edge is Running, Stop Microsoft Edge if Running
-    Write-Host "Is Microsoft Edge Running?"
-    if((get-process "msedge" -ea SilentlyContinue) -eq $Null){ 
-        echo "Not Running" 
-    }
-    else{ 
-    echo "Running, Stopping Process"
-    Stop-Process -processname "msedge"
-        }
-    # Tweak Microsoft Edge
-    Write-Host "Disabling Guest Mode"
-    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserGuestModeEnabled /t REG_DWORD /d 0
-    Write-Host "Disabling Add Profile"
-    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserAddProfileEnabled /t REG_DWORD /d 0
-    Write-Host "Disabling Incognito"
-    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v InPrivateModeAvailability /t REG_DWORD /d 1
- } 
-        }
-              }  
- mainmenu  
-   if ($answer -eq 8){
+    if ($answer -eq 6){
     Clear-Host
     # Complete All of the Above
     Write-Output "Installating Blocker Software AIO"
     $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Installations%20(Blockers).ps1"
     Invoke-Expression $($ScriptFromGithHub.Content)
  }
+  if ($answer -eq 7){
+    # Modify Browser Settings (Filtering)
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/BrowserSettings.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ } 
+ if ($answer -eq 7){
+    # Previous Menu
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ } 
  if ($answer -eq 9){areyousure} 
  else {write-host -ForegroundColor red "Invalid Selection"  
        sleep 5  
