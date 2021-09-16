925f9646243d62896f60f38cb2f87f6e9b1d5227
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-16 14:43:04 +0200

    Add files via upload

---
+++ Script Files/BrowserSettings.ps1
@@ -0,0 +1,77 @@
+  if ($answer -eq 6){
+    Clear-Host
+ function mainmenu{  
+ Clear-Host  
+ echo "---------------------------------------------------------"  
+ echo "     Modify Browser Settings (Filtering)"
+ echo ""
+ echo ""   
+ echo "    1. Modify Chrome Settings"
+ echo "    2. Modify Microsoft Edge Settings" 
+ echo ""
+ echo "    3. Previous Menu"
+ echo "" 
+ echo ""
+ echo "---------------------------------------------------------"  
+ $answer = read-host "Please Make a Selection"  
+
+  if ($answer -eq 1){
+    Clear-Host
+    # Modify Chrome Settings
+    Write-Host Is chrome Installed?
+    $w64=Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | where-Object DisplayName -like 'google chrome*'
+    $w32=Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*  | where-Object DisplayName -like 'google chrome*'
+    if ($w64 -or $w32)
+    {
+    Write-output "Google Chrome is already installed on your machine."
+    }
+    Else{
+    Write-Output "Google Chrome is not installed on your machine."
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Chrome.ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+    } 
+    # Check if Chrome is Running, Stop Chrome if Running
+    Write-Host "Is Chrome Running?"
+    if((get-process "chrome" -ea SilentlyContinue) -eq $Null){ 
+        echo "Not Running" 
+    }
+    else{ 
+    echo "Running, Stopping Process"
+    Stop-Process -processname "chrome"
+        }
+    # Tweak Chrome
+    Write-Host "Disabling Guest Mode"
+    REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserGuestModeEnabled /t REG_DWORD /d 0
+    #Write-Host "Disabling Add Profile"
+    #REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v BrowserAddPersonEnabled /t REG_DWORD /d 0
+    Write-Host "Disabling Incognito"
+    REG ADD HKLM\SOFTWARE\Policies\Google\Chrome /v IncognitoModeAvailability /t REG_DWORD /d 1
+}
+  if ($answer -eq 2){
+    Clear-Host
+    # Check if Microsoft Edge is Running, Stop Microsoft Edge if Running
+    Write-Host "Is Microsoft Edge Running?"
+    if((get-process "msedge" -ea SilentlyContinue) -eq $Null){ 
+        echo "Not Running" 
+    }
+    else{ 
+    echo "Running, Stopping Process"
+    Stop-Process -processname "msedge"
+        }
+    # Tweak Microsoft Edge
+    Write-Host "Disabling Guest Mode"
+    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserGuestModeEnabled /t REG_DWORD /d 0
+    Write-Host "Disabling Add Profile"
+    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v BrowserAddProfileEnabled /t REG_DWORD /d 0
+    Write-Host "Disabling Incognito"
+    REG ADD HKLM\SOFTWARE\Policies\Microsoft\Edge /v InPrivateModeAvailability /t REG_DWORD /d 1
+ } 
+   if ($answer -eq 3){
+    Clear-Host
+    # Previous Menu
+    $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Content%20Blockers%20(Adult%2C%20Social%2C%20Gambling%2Cetc).ps1
+    Invoke-Expression $($ScriptFromGithHub.Content)
+ } 
+        }
+              }  
+ mainmenu  
