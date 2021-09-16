60cdfc5bad16b2aac67307a8e9259c46e170ff47
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-16 15:05:09 +0200

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -34,6 +34,7 @@
  if ($answer -eq 2){
     Clear-Host
     Remove-Item "C:\ConfigureDomainPolicies.ps1"
+    Clear-Host
     # Setup and configure for Domain Policies
     Write-Output "Downloading ConfigureDomainPolicies Script File"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Setup%20and%20Configure%20for%20Domain%20Policies.ps1" -OutFile C:\ConfigureDomainPolicies.ps1
