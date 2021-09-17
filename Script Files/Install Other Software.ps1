ca0a668c2c082598a93b3600ca7c1c7aede4c1aa
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-17 14:41:57 +0200

    Update Install Other Software.ps1

--- Script Files/Install Other Software.ps1
+++ Script Files/Install Other Software.ps1
@@ -121,7 +121,7 @@
      Clear-Host
     # Google Drive
     Write-Host "Downloading Google Drive"
-    Invoke-Webrequest "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe" - OutFile "C:\GoogleDriveSetup.exe"
+    Invoke-WebRequest -Uri "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe" -OutFile "C:\GoogleDriveSetup.exe"
     PAUSE
     Write-Host "Installing Google Drive"
     Start-Process -Wait -FilePath "C:\GoogleDriveSetup.exe"
