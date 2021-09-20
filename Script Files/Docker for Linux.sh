af5c340a70fa111a428f3cd797a4dc0dfe068147
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 17:09:25 +0200

    Update Docker for Linux.sh

--- Script Files/Docker for Linux.sh
+++ Script Files/Docker for Linux.sh
@@ -6,8 +6,8 @@ sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
 # Install Docker 
 echo "Installing Docker"
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
