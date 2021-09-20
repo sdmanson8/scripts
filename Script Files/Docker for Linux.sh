f9b445a9c79ace39b84780c6a616c6f1532592ba
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 19:17:06 +0200

    Update Docker for Linux.sh

--- Script Files/Docker for Linux.sh
+++ Script Files/Docker for Linux.sh
@@ -2,14 +2,19 @@
 
 # Edit Sudoers File
 echo "Add user to Sudoers File"
+wait 5
 sudo visudo
 
 # Update FileSystem
+echo ""
 echo "Updating FileSystem"
+wait 5
 sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
 # Install Docker
+echo ""
 echo "Installing Docker"
+wait 5
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -17,10 +22,19 @@ sudo apt-get update -y
 sudo apt-get install docker-ce -y
 
 # Install Docker-Compose
+echo ""
 echo "Installing Docker-Compose"
-COMPOSE_VER=$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")
-curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VER/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+wait 5
+install-docker-compose () {
+    fname="docker-compose-$(uname -s)-$(uname -m)"
+    fbin="~/.local/bin/docker-compose"
+    repos_dl="https://github.com/docker/compose/releases/download"
+    repos_api="https://api.github.com/repos/docker/compose/releases/latest"
+    version=$(curl -sSL $repos_api | jq -r '. | .tag_name')
+    /bin/rm -f "$fbin"
+    curl -sSL $repos_dl/"$version"/"$fname" -o "$fbin"
+    chmod +x $fbin
+}
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 # Install docker-cleanup command
@@ -29,6 +43,7 @@ git clone https://gist.github.com/76b450a0c986e576e98b.git
 cd 76b450a0c986e576e98b
 sudo mv docker-cleanup /usr/local/bin/docker-cleanup
 sudo chmod +x /usr/local/bin/docker-cleanup
+sudo rm -R /tmp/76b450a0c986e576e98b -f
 
 # Add User to Docker Group
 sudo usermod -aG docker ${USER}
