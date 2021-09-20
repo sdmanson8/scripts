905a018a8e4ae721463018999ef2801b94656510
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 19:35:04 +0200

    Update Docker for Linux.sh

--- Script Files/Docker for Linux.sh
+++ Script Files/Docker for Linux.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Edit Sudoers File
-echo "Add user to Sudoers File"
-wait 5
+printf 'Add Current User to Sudoers File\n\n'
+start-sleep -seconds 5
 sudo visudo
 
 # Update FileSystem
-echo ""
-echo "Updating FileSystem"
-wait 5
+printf '\nUpdating FileSystem\n\n'
+start-sleep -seconds 5
 sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
 # Docker
+printf '\nInstalling Docker\n\n'
+start-sleep -seconds 5
 sudo apt remove --yes docker docker-engine docker.io containerd runc || true
 sudo apt update
 sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
@@ -24,9 +25,11 @@ sudo systemctl enable docker
 printf '\nDocker installed successfully\n\n'
 
 printf 'Waiting for Docker to start...\n\n'
-sleep 5
+start-sleep -seconds 2
 
 # Docker Compose
+printf '\nInstalling Docker-Compose\n\n'
+start-sleep -seconds 5
 sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
@@ -42,4 +45,5 @@ sudo chmod +x /usr/local/bin/docker-cleanup
 sudo rm -R /tmp/76b450a0c986e576e98b -f
 
 # Add User to Docker Group
+printf '\nAdding Current User to Docker Group\n\n'
 sudo usermod -aG docker ${USER}
