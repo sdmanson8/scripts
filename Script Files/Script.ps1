9bac2cc419dcc67c8c8ab9f64e46134edefcf25c
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 17:55:40 +0200

    Update Script.ps1

--- Script Files/Script.ps1
+++ Script Files/Script.ps1
@@ -36,7 +36,7 @@
     Clear-Host
     # Docker for Linux
     Write-Output "Docker for Linux"
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Docker%20for%20Linux.sh"
+    $ScriptFromGithHub = Invoke-WebRequest "https://github.com/sdmanson8/scripts/blob/main/Script%20Files/Docker%20for%20Linux.ps1"
     Invoke-Expression $($ScriptFromGithHub.Content)
 }
  if ($answer -eq 3){areyousure} 
