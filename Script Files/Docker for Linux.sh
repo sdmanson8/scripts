0608340539783d2c5a66c537569a2d75f33edef4
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 17:09:40 +0200

    Update Docker for Linux.sh

--- Script Files/Docker for Linux.sh
+++ Script Files/Docker for Linux.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 # Update FileSystem
-echo "Updating FileSystem"
-sudo apt-get update -y && sudo apt-get dist-upgrade -y
+#echo "Updating FileSystem"
+#sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
 # Install Docker 
 echo "Installing Docker"
