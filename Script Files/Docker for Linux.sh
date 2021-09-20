f25340ea501be98fc4b35249f6fcdfffe2d32649
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-20 19:29:34 +0200

    Update Docker for Linux.sh

--- Script Files/Docker for Linux.sh
+++ Script Files/Docker for Linux.sh
@@ -11,33 +11,29 @@ echo "Updating FileSystem"
 wait 5
 sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
-# Install Docker
-echo ""
-echo "Installing Docker"
-wait 5
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install docker-ce -y
+# Docker
+sudo apt remove --yes docker docker-engine docker.io containerd runc || true
+sudo apt update
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo apt update
+sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+printf '\nDocker installed successfully\n\n'
 
-# Install Docker-Compose
-echo ""
-echo "Installing Docker-Compose"
-wait 5
-install-docker-compose () {
-    fname="docker-compose-$(uname -s)-$(uname -m)"
-    fbin="~/.local/bin/docker-compose"
-    repos_dl="https://github.com/docker/compose/releases/download"
-    repos_api="https://api.github.com/repos/docker/compose/releases/latest"
-    version=$(curl -sSL $repos_api | jq -r '. | .tag_name')
-    /bin/rm -f "$fbin"
-    curl -sSL $repos_dl/"$version"/"$fname" -o "$fbin"
-    chmod +x $fbin
-}
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
+
+# Docker Compose
+sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+printf '\nDocker Compose installed successfully\n\n'
 
 # Install docker-cleanup command
+mkdir /usr/local/bin/docker-cleanup
 cd /tmp
 git clone https://gist.github.com/76b450a0c986e576e98b.git
 cd 76b450a0c986e576e98b
