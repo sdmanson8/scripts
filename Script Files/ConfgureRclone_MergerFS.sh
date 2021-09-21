737fc4101dd7f76f2c5a810b539af95394d6eefd
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-21 15:59:09 +0200

    Update ConfgureRclone_MergerFS.sh

--- Script Files/ConfgureRclone_MergerFS.sh
+++ Script Files/ConfgureRclone_MergerFS.sh
@@ -7,12 +7,12 @@ sudo mkdir ~/scripts
 
 # Download Rclone Systemd File
 printf '\nDownloading Rclone Systemd File.. Please Wait\n\n'
-sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/rclone.service -o /etc/systemd/system/rclone.service
+sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/rclone.service -P /etc/systemd/system/rclone.service
 sleep 1s
 
 # Download MergerFS Systemd File
 printf '\nDownloading MergerFS Systemd File.. Please Wait\n\n'
-sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/rclone.service -o /etc/systemd/system/rclone.service
+sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/mergerfs.service -P /etc/systemd/system/mergerfs.service
 sleep 1s
 
 # Enable Systemd Services for Rclone and Mergerfs
@@ -23,5 +23,5 @@ sleep 1s
 
 # Download Rclone Upload Script
 printf '\nDownloading Rclone Upload Script.. Please Wait\n\n'
-sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/rclone-upload.sh -o ~/scripts/rclone-upload.sh
+sudo wget https://github.com/sdmanson8/scripts/blob/main/Script%20Files/rclone-upload.sh -P ~/scripts/rclone-upload.sh
 sleep 1s
