05e6edf418d6663dc4462764399b8b4dc62140ff
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-21 17:05:28 +0200

    Update rclone-upload.sh

--- Script Files/rclone-upload.sh
+++ Script Files/rclone-upload.sh
@@ -4,10 +4,13 @@
 # Type crontab -e and add line below (without #) and with correct path to the script
 # * * * * * /pathtoscript/scripts/rclone-upload.sh >/dev/null 2>&1
 
+HOME=/home/sheldon
+USER=sheldon
+
 set -e
 
 # RClone Config file
-RCLONE_CONFIG=~/.config/rclone/rclone.conf; export RCLONE_CONFIG
+RCLONE_CONFIG=$HOME/.config/rclone/rclone.conf; export RCLONE_CONFIG
 RCLONE_USER_AGENT=$USER; export RCLONE_USER_AGENT
 
 # Local Drive - This must be a local mount point on your server that is used for the source of files
@@ -15,7 +18,6 @@ RCLONE_USER_AGENT=$USER; export RCLONE_USER_AGENT
 # and DELETE YOUR FILES!
 # Make sure to set this to the local path you are moving from!!
 LOCAL=/mnt/local
-HOME=~/
 REMOTE="gdrive"
 
 # Bandwidth limits: specify the desired bandwidth in kBytes/s, or use a suffix b|k|M|G. Or 'off' or '0' for unlimited.  The script uses --drive-stop-on-upload-limit which stops the script if the 750GB/day limit is achieved, so you no longer have to slow 'trickle' your files all day if you don't want to e.g. could just do an unlimited job overnight.
@@ -37,7 +39,7 @@ if /bin/findmnt $LOCAL -o FSTYPE -n | grep fuse; then
 echo "FUSE file system found, exiting..."; exit 1; fi
 
 # Move older local files to the cloud...
- $HOME/bin/rclone move $LOCAL $REMOTE: \
+ usr/bin/rclone move $LOCAL $REMOTE: \
  --log-file $HOME/scripts/rclone-upload.log \
  -v \
  --exclude-from $HOME/scripts/excludes \
