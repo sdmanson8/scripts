77017e6a565eccbadce490ac3017b9df44be29c2
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-21 14:25:17 +0200

    Update ConfgureRclone_MergerFS.sh

--- Script Files/ConfgureRclone_MergerFS.sh
+++ Script Files/ConfgureRclone_MergerFS.sh
@@ -6,10 +6,10 @@ clear
 printf '\nChange Root Password (DO NOT USE A EASY PASSWORD)\n\n'
 sleep 2s
 sudo passwd root &
-read -p
+read -p "Press any key to continue... " -n1 -s
 
 #Run Rclone Config
 printf '\nPreparing to Run Rclone Config.. Please Wait\n\n'
 sleep 1s
 sudo rclone config 
-read -p
+read -p "Press any key to continue... " -n1 -s
