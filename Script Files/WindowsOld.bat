1c253c163a2bd93cfdb03d5def2c21726688f4fc
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-26 10:59:41 +0200

    Update WindowsOld.bat

--- Script Files/WindowsOld.bat
+++ Script Files/WindowsOld.bat
@@ -1,3 +1,7 @@
 takeown /F c:\Windows.old\* /R /A /D Y
 cacls c:\Windows.old\*.* /T /grant administrators:F
 rmdir /S /Q c:\Windows.old
+
+dism /online /cleanup-image /AnalyzeComponentStore
+Start-Sleep -Seconds 5
+dism /online /cleanup-image /StartComponentCleanup /ResetBase
