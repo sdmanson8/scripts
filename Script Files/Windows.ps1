3d8935362a3025182d7089fe9758e870a31e4ed0
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-27 17:31:34 +0200

    Update Windows.ps1

--- Script Files/Windows.ps1
+++ Script Files/Windows.ps1
@@ -1,122 +1,82 @@
-﻿ #Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
+#requires -version 5.0
+#Requires -RunAsAdministrator
+
+ #Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
  function areyousure {$areyousure = read-host "Are you sure you want to exit? (y/n)"  
            if ($areyousure -eq "y"){exit}  
            if ($areyousure -eq "n"){mainmenu}  
-                                } 
-  #Mainmenu function. Contains the screen output for the menu and waits for and handles user input.  
+           else {write-host -foregroundcolor red "Invalid Selection"    
+                 areyousure  
+                }  
+                     } 
+ 
+ #Mainmenu function. Contains the screen output for the menu and waits for and handles user input.  
  function mainmenu{  
- Clear-Host  
+ clear  
  echo "---------------------------------------------------------"  
- echo "        Install Other Software"
+ echo "                     WINDOWS SCRIPT MENU"
  echo ""
  echo ""  
- echo "    1. Dashlane"
- echo "    2. Grammarly"
- echo "    3. Google Chrome"
- echo "    4. Firefox"
- echo "    5. Microsoft Edge"
- echo "    6. Google Drive"
- echo "    7. Tree Size" 
+ echo "    1. Windows First Run (Post Clean Install) Tweaks"
+ echo "    2. Setup and configure for Domain Policies"
+ echo "    3. Content Blockers (Adult, Social, Gambling,etc)"
+ echo "    4. Install HP Software + Other Software"
  echo ""
- echo "    8. Install All of the Above"
- echo " (Choose which to install Google Chrome / Firefox)"
+ echo "    5. Previous Menu"
  echo ""
- echo "    9. Previous Menu"
- echo ""
- echo "    10. exit" 
+ echo "    6. exit" 
  echo "" 
  echo ""
  echo "---------------------------------------------------------"  
  $answer = read-host "Please Make a Selection"  
- 
-  if ($answer -eq 1){
+ if ($answer -eq 1){
     Clear-Host
-    # Dashlane Desktop
-    Write-Host "Opening Webpage to Download Dashlane Desktop"
-    Start-Process "https://www.dashlane.com/download/desktop#downloaded"
-    PAUSE
-    Write-Host "Installing Dashlane Desktop"
-    Start-Process -FilePath "$env:USERPROFILE\Downloads\DashlaneInst.exe"
-    PAUSE
-    Remove-Item "$env:USERPROFILE\Downloads\DashlaneInst.exe"
-    Write-Host "Opening Webpage to Setup Dashlane Addon for Microsoft Edge"
-    Start-Process "https://microsoftedge.microsoft.com/addons/detail/dashlane-password-manag/gehmmocbbkpblljhkekmfhjpfbkclbph"
- }
-  if ($answer -eq 2){
-    Clear-Host
-    # Grammarly
-    Write-Output "Downloading Grammarly for Windows"
-    Invoke-WebRequest -Uri "https://download-editor.grammarly.com/windows/GrammarlySetup.exe" -OutFile "C:\GrammarlySetup.exe"
-    Write-Host "Installing Grammarly"
-    Start-Process -FilePath "C:\GrammarlySetup.exe"
-    PAUSE
-    Remove-Item "C:\GrammarlySetup.exe"
-    Write-Output "Downloading Grammarly for Microsoft Office"
-    Invoke-WebRequest -Uri "https://download-office.grammarly.com/latest/GrammarlyAddInSetup.exe" -OutFile "C:\GrammarlyAddInSetup.exe"
-    Write-Host "Installing Grammarly for Microsoft Office"
-    Start-Process -Wait -FilePath "C:\GrammarlyAddInSetup.exe"
-    PAUSE
-    Remove-Item "C:\GrammarlyAddInSetup.exe"
-    Write-Host "Opening Webpage to Setup Grammarly Addon for Microsoft Edge"
-    Start-Process "https://microsoftedge.microsoft.com/addons/detail/grammarly-for-microsoft-e/cnlefmmeadmemmdciolhbnfeacpdfbkd"
-
- }
-   if ($answer -eq 3){
-    Clear-Host
-    # Install Google Chrome
-    Write-Output "Installing Google Chrome"
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Chrome.ps1"
+    # Windows First Run (Post Clean Install) Tweaks
+    Write-Output "Windows First Run (Post Clean Install) Tweaks"
+    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Windows%20First%20Run%20Script.ps1"
     Invoke-Expression $($ScriptFromGithHub.Content)
- }
+ }  
+ if ($answer -eq 2){
+    Clear-Host
+    Remove-Item "C:\ConfigureDomainPolicies.ps1"
+    Clear-Host
+    # Setup and configure for Domain Policies
+    Write-Output "Downloading ConfigureDomainPolicies Script File"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Setup%20and%20Configure%20for%20Domain%20Policies.ps1" -OutFile C:\ConfigureDomainPolicies.ps1
+    # ConfigureDomainPolicies Script File
+    PAUSE
+    Write-Host Edit ConfigureDomainPolicies Script File
+    & "C:\Program Files\Notepad++\notepad++.exe" "C:\ConfigureDomainPolicies.ps1"
+    PAUSE
+    Write-Host Running ConfigureDomainPolicies Script
+    Clear-Host
+    & C:\ConfigureDomainPolicies.ps1
+    PAUSE
+    Write-Host Removing Leftover Files
+    Remove-Item "C:\ConfigureDomainPolicies.ps1"
+ }  
+  if ($answer -eq 3){
+    Clear-Host
+    # "Content Blockers (Adult, Social, Gambling,etc)"
+    Write-Output "Content Blockers (Adult, Social, Gambling,etc)"
+    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Content%20Blockers%20(Adult%2C%20Social%2C%20Gambling%2Cetc).ps1"
+    Invoke-Expression $($ScriptFromGithHub.Content)
+}
    if ($answer -eq 4){
     Clear-Host
-    # Install Firefox
-    Write-Output "Downloading Firefox"
-    Invoke-WebRequest -Uri "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -OutFile "C:\firefox-latest.exe"
-    Write-Host "Installing Firefox"
-    Start-Process -Wait -FilePath "C:\firefox-latest.exe"
-    Remove-Item "C:\firefox-latest.exe"
- }
-   if ($answer -eq 5){
-    Clear-Host
-    # Install Microsoft Edge
-    Write-Output "Downloading Microsoft Edge"
-    Invoke-WebRequest -Uri "https://c2rsetup.officeapps.live.com/c2r/downloadEdge.aspx?platform=Default&source=EdgeStablePage&Channel=Stable&language=en" -OutFile "C:\MicrosoftEdgeSetup.exe"
-    Write-Host "Installing Microsoft Edge"
-    Start-Process -Wait -FilePath "C:\MicrosoftEdgeSetup.exe"
-    Remove-Item "C:\MicrosoftEdgeSetup.exe"
- }
-   if ($answer -eq 6){
-     Clear-Host
-    # Google Drive
-    Write-Host "Downloading Google Drive"
-    Invoke-WebRequest -Uri "https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe" -OutFile "C:\GoogleDriveSetup.exe"
-    Write-Host "Installing Google Drive"
-    Start-Process -Wait -FilePath "C:\GoogleDriveSetup.exe"
-    Remove-Item "C:\GoogleDriveSetup.exe"
- }
-    if ($answer -eq 7){
-     Clear-Host
-    # Tree Size
-    Write-Host "Downloading Tree Size"
-    Invoke-WebRequest -Uri "https://downloads.jam-software.de/treesize_free/TreeSizeFreeSetup.exe" -OutFile "C:\TreeSizeFreeSetup.exe"
-    Write-Host "Installing Tree Size"
-    Start-Process -Wait -FilePath "C:\TreeSizeFreeSetup.exe"
-    Remove-Item "C:\TreeSizeFreeSetup.exe"
- }
-  if ($answer -eq 8){
-    Clear-Host
-    # Install All of the Above
-    Write-Output "Install All of the Above"
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/OtherSoftware.ps1"
+    # "Install HP Software and Drivers"
+    Write-Output "Install HP Software and Drivers"
+    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/InstallHPDriversandSoftware.ps1"
     Invoke-Expression $($ScriptFromGithHub.Content)
- }
-  if ($answer -eq 9){
+ }  
+  if ($answer -eq 5){
+    Clear-Host
     # Previous Menu
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script.ps1"
+    Write-Output "Previous Menu"
+    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1"
     Invoke-Expression $($ScriptFromGithHub.Content)
- }
- if ($answer -eq 12){areyousure} 
+}
+ if ($answer -eq 6){areyousure} 
  else {write-host -ForegroundColor red "Invalid Selection"  
        sleep 5  
        mainmenu  
