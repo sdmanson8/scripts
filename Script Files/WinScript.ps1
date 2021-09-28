2305c3428b860b121b35f285605844b38712bd06
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 15:16:35 +0200

    Update WinScript.ps1

--- Script Files/WinScript.ps1
+++ Script Files/WinScript.ps1
@@ -1,2 +1,4 @@
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1"
-    Invoke-Expression $($ScriptFromGithHub.Content)
+    Write-Host "Opening Script... Please Wait"
+    Start-Sleep -Seconds 1
+    Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1" -OutFile "C:\Script.ps1"
+
