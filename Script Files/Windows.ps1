ea1f7d15b68650610c459ba547b71a94eb71a38f
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 14:35:28 +0200

    Update Windows.ps1

--- Script Files/Windows.ps1
+++ Script Files/Windows.ps1
@@ -68,6 +68,28 @@ if ($w64 -or $w32)
         echo "" 
     }
     else{ 
+#requires -version 5.1
+#Calling Powershell as Admin and setting Execution Policy to Bypass to avoid Cannot run Scripts error
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+#Is Powershell 7 Installed
+  $w64=Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
+  $w32=Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"  | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
+if ($w64 -or $w32)
+{
+  Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+# Check if Windows Terminal is Running, Stop Windows Terminal if Running
+    if((get-process "WindowsTerminal" -ea SilentlyContinue) -eq $Null){ 
+        echo "" 
+    }
+    else{ 
+    Stop-Process -processname "WindowsTerminal"
+        }
+# Check if CMD is Running, Stop Windows Terminal if Running
+    if((get-process "cmd" -ea SilentlyContinue) -eq $Null){ 
+        echo "" 
+    }
+    else{ 
     Stop-Process -processname "cmd"
         }
     # Check if Powershell 7 is Running, Stop Powershell 7 if Running
@@ -87,7 +109,7 @@ Else{
     else{ 
     Stop-Process -processname "WindowsTerminal"
         }
-# Check if CMD is Running, Stop Windows Terminal if Running
+# Check if CMD is Running, Stop CMD if Running
     if((get-process "cmd" -ea SilentlyContinue) -eq $Null){ 
         echo "" 
     }
