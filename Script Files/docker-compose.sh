79b6c87f35e4303a707d00218647200f9c2ea04e
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 21:36:13 +0200

    Update docker-compose.sh

--- Script Files/docker-compose.sh
+++ Script Files/docker-compose.sh
@@ -8,6 +8,7 @@
 printf '\nRemoving old files.. Please Wait\n\n'
 sleep 1s
 rm ~/docker/RealTraefikCert.yml
+rm ~/docker/traefik/rules/middlewares.yml
 
 # Downloading docker-compose.yml file
 printf '\nDownloading docker-compose.yml file.. Please Wait\n\n'
@@ -18,6 +19,25 @@ wget https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/doc
 printf '\nCreating macvlan docker network.. Please Wait\n\n'
 sleep 1s
 docker network create -d macvlan -o parent="$(ip route get 8.8.8.8 | awk -- '{printf $5}')" br0
+
+# Downloading Container Files
+printf '\nDownloading Container Files.. Please Wait\n\n'
+sleep 1s
+wget https://github.com/sdmanson8/scripts/archive/refs/heads/main.zip -O ~/main.zip
+
+# Extracting Container Files
+printf '\Extracting Container Files.. Please Wait\n\n'
+sleep 1s
+unzip -d ~/Main ~/main.zip
+rm -f main.zip
+clear
+
+# Copying Container Files to ~/docker
+printf '\nCopying Container Files to ~/docker.. Please Wait\n\n'
+sleep 1s
+cd ~/Main/scripts-main/Script\ Files
+cp -r Authelia/ prometheus/ qbittorrent/ telegraf/ traefik/ transmission/ ~/docker/
+cd ~/ && rm -Rf Main/
 
 cd ~/docker
 clear
