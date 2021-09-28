84339370761300ee729fad432b98741f3e05eb52
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 14:31:45 +0200

    Update Windows First Run Script.ps1

--- Script Files/Windows First Run Script.ps1
+++ Script Files/Windows First Run Script.ps1
@@ -1,4 +1,4 @@
-#requires -version 5.0
+#requires -version 5.1
 #Calling Powershell as Admin and setting Execution Policy to Bypass to avoid Cannot run Scripts error
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
 {  
@@ -57,7 +57,10 @@ Else{
     }
 }
 
- #Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
+Clear-Host
+#Requires -RunAsAdministrator
+
+#Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
  function areyousure {$areyousure = read-host "Are you sure you want to exit? (y/n)"  
            if ($areyousure -eq "y"){exit}  
            if ($areyousure -eq "n"){mainmenu}  
