732e64743f7c22c791b98b396f8cde932fe8bee0
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 16:00:19 +0200

    Create docker-compose.sh

---
+++ Script Files/docker-compose.sh
@@ -0,0 +1,14 @@
+# Script File
+
+# Remove Old files
+printf '\nRemoving old files.. Please Wait\n\n'
+sleep 1s
+rm ~/docker/RealTraefikCert.yml
+
+# #Downloading docker-compose.yml file
+printf '\nDownloading docker-compose.yml file.. Please Wait\n\n'
+sleep 1s
+wget https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/docker-compose.yml -O ~/docker/docker-compose.yml
+
+cd ~/docker
+clear
