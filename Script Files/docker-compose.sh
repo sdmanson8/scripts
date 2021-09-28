40f278c3d2423506dcd6f8e405f4d50ff30c7fd6
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 21:42:47 +0200

    Update docker-compose.sh

--- Script Files/docker-compose.sh
+++ Script Files/docker-compose.sh
@@ -36,8 +36,8 @@ clear
 #printf '\nCopying Container Files to ~/docker.. Please Wait\n\n'
 #sleep 1s
 #cd ~/Main/scripts-main/Script\ Files
-#cp -r Authelia/ prometheus/ qbittorrent/ telegraf/ traefik/ transmission/ ~/docker/
-#cd ~/ && rm -Rf Main/
+#cp -r Authelia/ prometheus/ qbittorrent/ telegraf/ traefik/ transmission/ ~/docker/ && cd ~/ && rm -Rf Main/
+
 
 cd ~/docker
 clear
