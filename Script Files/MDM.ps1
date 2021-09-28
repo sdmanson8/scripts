856e0075714f621c8dcabcf92877b1d0ffd14162
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 11:02:23 +0200

    Update MDM.ps1

--- Script Files/MDM.ps1
+++ Script Files/MDM.ps1
@@ -4,14 +4,9 @@ Write-Host Adding Domain User to Local Admin group
 Add-LocalGroupMember -Group "Administrators" -Member "example\user"
 
 PAUSE
-$msg     = 'Do you want to do MDM Enrolment? [Type Y/N]'
-do {
-            $response = Read-Host -Prompt $msg
-            if ($response -eq 'y') {
-            #MDM Enrolment
-            Write-Host Starting Device Enrolment
-            Start-Process ms-device-enrollment:?mode=mdm"&"username=user@example.com}
-} until ($response -eq 'n')
+#MDM Enrolment
+Write-Host Starting Device Enrolment
+Start-Process ms-device-enrollment:?mode=mdm"&"username=user@example.com}
 
 PAUSE
 Write-Host "Opening Manage Bitlocker in Control Panel"
