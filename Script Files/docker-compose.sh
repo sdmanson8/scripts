e6b816cac0cbb2aee92b74b6d5611875656bf5af
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-09-28 16:39:58 +0200

    Update docker-compose.sh

--- Script Files/docker-compose.sh
+++ Script Files/docker-compose.sh
@@ -9,10 +9,15 @@ printf '\nRemoving old files.. Please Wait\n\n'
 sleep 1s
 rm ~/docker/RealTraefikCert.yml
 
-# #Downloading docker-compose.yml file
+# Downloading docker-compose.yml file
 printf '\nDownloading docker-compose.yml file.. Please Wait\n\n'
 sleep 1s
 wget https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/docker-compose.yml -O ~/docker/docker-compose.yml
+
+# Create macvlan docker network
+printf '\nCreating macvlan docker network.. Please Wait\n\n'
+sleep 1s
+docker network create -d macvlan -o parent="$(ip route get 8.8.8.8 | awk -- '{printf $5}')" br0
 
 cd ~/docker
 clear
