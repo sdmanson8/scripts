7d85c5e62289fedc9a03132c8555e4b5b768cd1d
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-01 19:06:32 +0200

    Update Main Script.ps1

--- Script Files/Main Script.ps1
+++ Script Files/Main Script.ps1
@@ -1,2 +1,2 @@
-    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1"
+    $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1" -UseBasicParsing
     Invoke-Expression $($ScriptFromGithHub.Content)
