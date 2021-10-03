b30d51ade154adff978a37816b46499e3d024e73
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-02 23:47:20 -0700

    Update bloatware.ps1

--- Script Files/bloatware.ps1
+++ Script Files/bloatware.ps1
@@ -1,122 +1,126 @@
-﻿Write-Output "Uninstalling default apps"
-$apps = @(
-    # default Windows 10 apps
-    "Microsoft.549981C3F5F10" #Cortana
-    "Microsoft.3DBuilder"
-    "Microsoft.Appconnector"
-    "Microsoft.BingFinance"
-    "Microsoft.BingNews"
-    "Microsoft.BingSports"
-    "Microsoft.BingTranslator"
-    "Microsoft.BingWeather"
-    "Microsoft.FreshPaint"
-    "Microsoft.GamingServices"
-    "Microsoft.Microsoft3DViewer"
-    "Microsoft.MicrosoftOfficeHub"
-    "Microsoft.MicrosoftPowerBIForWindows"
-    "Microsoft.MicrosoftSolitaireCollection"
-    "Microsoft.MicrosoftStickyNotes"
-    "Microsoft.MinecraftUWP"
-    "Microsoft.NetworkSpeedTest"
-    "Microsoft.Office.OneNote"
-    "Microsoft.People"
-    "Microsoft.Print3D"
-    "Microsoft.SkypeApp"
-    "Microsoft.Wallet"
-    "Microsoft.Windows.Photos"
-    "Microsoft.WindowsAlarms"
-    #"Microsoft.WindowsCalculator"
-    "Microsoft.WindowsCamera"
-    "microsoft.windowscommunicationsapps"
-    "Microsoft.WindowsMaps"
-    "Microsoft.WindowsPhone"
-    "Microsoft.WindowsSoundRecorder"
-    "Microsoft.Xbox.TCUI"
-    "Microsoft.XboxApp"
-    "Microsoft.XboxGameOverlay"
-    "Microsoft.XboxGamingOverlay"
-    "Microsoft.XboxSpeechToTextOverlay"
-    "Microsoft.YourPhone"
-    "Microsoft.ZuneMusic"
-    "Microsoft.ZuneVideo"
-
-    # Threshold 2 apps
-    "Microsoft.CommsPhone"
-    "Microsoft.ConnectivityStore"
-    "Microsoft.GetHelp"
-    "Microsoft.Getstarted"
-    "Microsoft.Messaging"
-    "Microsoft.Office.Sway"
-    "Microsoft.OneConnect"
-    "Microsoft.WindowsFeedbackHub"
-
-    # Creators Update apps
-    "Microsoft.Microsoft3DViewer"
-    #"Microsoft.MSPaint"
-
-    #Redstone apps
-    "Microsoft.BingFoodAndDrink"
-    "Microsoft.BingHealthAndFitness"
-    "Microsoft.BingTravel"
-    "Microsoft.WindowsReadingList"
-
-    # Redstone 5 apps
-    "Microsoft.MixedReality.Portal"
-    "Microsoft.ScreenSketch"
-    "Microsoft.XboxGamingOverlay"
-
-    # non-Microsoft
-    "2FE3CB00.PicsArt-PhotoStudio"
-    "46928bounde.EclipseManager"
-    "4DF9E0F8.Netflix"
-    "613EBCEA.PolarrPhotoEditorAcademicEdition"
-    "6Wunderkinder.Wunderlist"
-    "7EE7776C.LinkedInforWindows"
-    "89006A2E.AutodeskSketchBook"
-    "9E2F88E3.Twitter"
-    "A278AB0D.DisneyMagicKingdoms"
-    "A278AB0D.MarchofEmpires"
-    "ActiproSoftwareLLC.562882FEEB491" # next one is for the Code Writer from Actipro Software LLC
-    "CAF9E577.Plex"  
-    "ClearChannelRadioDigital.iHeartRadio"
-    "D52A8D61.FarmVille2CountryEscape"
-    "D5EA27B7.Duolingo-LearnLanguagesforFree"
-    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
-    "DolbyLaboratories.DolbyAccess"
-    "DolbyLaboratories.DolbyAccess"
-    "Drawboard.DrawboardPDF"
-    "Facebook.Facebook"
-    "Fitbit.FitbitCoach"
-    "Flipboard.Flipboard"
-    "GAMELOFTSA.Asphalt8Airborne"
-    "KeeperSecurityInc.Keeper"
-    "NORDCURRENT.COOKINGFEVER"
-    "PandoraMediaInc.29680B314EFC2"
-    "Playtika.CaesarsSlotsFreeCasino"
-    "ShazamEntertainmentLtd.Shazam"
-    "SlingTVLLC.SlingTV"
-    "SpotifyAB.SpotifyMusic"
-    "TheNewYorkTimes.NYTCrossword"
-    "ThumbmunkeysLtd.PhototasticCollage"
-    "TuneIn.TuneInRadio"
-    "WinZipComputing.WinZipUniversal"
-    "XINGAG.XING"
-    "flaregamesGmbH.RoyalRevolt2"
-    "king.com.*"
-    "king.com.BubbleWitch3Saga"
-    "king.com.CandyCrushSaga"
-    "king.com.CandyCrushSodaSaga"
-
-    # apps which other apps depend on
-    "Microsoft.Advertising.Xaml"
-)
-
-$appxprovisionedpackage = Get-AppxProvisionedPackage -Online
-
-foreach ($app in $apps) {
-    Write-Output "Trying to remove $app"
-    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
-
-    ($appxprovisionedpackage).Where( {$_.DisplayName -EQ $app}) |
-    Remove-AppxProvisionedPackage -Online
-}
+$AppList = "Microsoft.3DBuilder",
+           "Microsoft.Appconnector",
+           "Microsoft.BingFinance",
+           "Microsoft.BingNews",
+           "Microsoft.BingSports",
+           "Microsoft.BingTranslator",
+           "Microsoft.BingWeather",
+           "Microsoft.FreshPaint",
+           "Microsoft.DesktopAppInstaller",
+           "Microsoft.Getstarted",
+           "Microsoft.GetHelp",
+           "Microsoft.Messaging",
+           "Microsoft.Microsoft3DViewer",
+           "Microsoft.MicrosoftOfficeHub",
+           "Microsoft.MicrosoftPowerBIForWindows",
+           "Microsoft.MicrosoftSolitaireCollection",
+           "Microsoft.MicrosoftStickyNotes",
+           "Microsoft.MinecraftUWP",
+           "Microsoft.NetworkSpeedTest",
+           "Microsoft.WindowsPhone",
+           "Microsoft.CommsPhone",
+           "Microsoft.ConnectivityStore",
+           "Microsoft.Office.Sway",
+           "Microsoft.BingFoodAndDrink",
+           "Microsoft.BingTravel",
+           "Microsoft.BingHealthAndFitness",
+           "9E2F88E3.Twitter",
+           "PandoraMediaInc.29680B314EFC2",
+           "Flipboard.Flipboard",
+           "ShazamEntertainmentLtd.Shazam",
+           "king.com.CandyCrushSaga",
+           "king.com.CandyCrushSodaSaga",
+           "king.com.",
+           "ClearChannelRadioDigital.iHeartRadio",
+           "4DF9E0F8.Netflix",
+           "6Wunderkinder.Wunderlist",
+           "Drawboard.DrawboardPDF",
+           "22StokedOnIt.NotebookPro",
+           "2FE3CB00.PicsArt-PhotoStudio",
+           "41038Axilesoft.ACGMediaPlayer",
+           "5CB722CC.SeekersNotesMysteriesofDarkwood",
+           "7458BE2C.WorldofTanksBlitz",
+           "D52A8D61.FarmVille2CountryEscape",
+           "TuneIn.TuneInRadio",
+           "GAMELOFTSA.Asphalt8Airborne",
+           "TheNewYorkTimes.NYTCrossword",
+           "DB6EA5DB.CyberLinkMediaSuiteEssentials",
+           "Facebook.Facebook",
+           "flaregamesGmbH.RoyalRevolt2",
+           "Playtika.CaesarsSlotsFreeCasino",
+           "A278AB0D.MarchofEmpires",
+           "KeeperSecurityInc.Keeper",
+           "ThumbmunkeysLtd.PhototasticCollage",
+           "INGAG.XING",
+           "89006A2E.AutodeskSketchBook",
+           "D5EA27B7.Duolingo-LearnLanguagesforFree",
+           "46928bounde.EclipseManager",
+           "ActiproSoftwareLLC.562882FEEB49",
+           "DolbyLaboratories.DolbyAccess",
+           "SpotifyAB.SpotifyMusic",
+           "A278AB0D.DisneyMagicKingdoms",
+           "WinZipComputing.WinZipUniversal",
+           "Microsoft.MSPaint",
+           "Microsoft.Office.OneNote",
+           "Microsoft.OneConnect",
+           "Microsoft.People",
+           "Microsoft.Print3D",
+           "Microsoft.SkypeApp",
+           "Microsoft.Wallet",
+           "Microsoft.Windows.Photos",
+           "Microsoft.WindowsAlarms",
+           "Microsoft.WindowsCamera",
+           "Microsoft.windowscommunicationsapps",
+           "Microsoft.WindowsFeedbackHub",
+           "Microsoft.WindowsMaps",
+           "Microsoft.WindowsSoundRecorder",
+           "Microsoft.XboxApp",
+           "Microsoft.Xbox.TCUI",
+           "Microsoft.ZuneMusic",
+           "Microsoft.ZuneVideo",
+           "828B5831.HiddenCityMysteryofShadows",
+           "king.com.BubbleWitch3Saga",
+           "Fitbit.FitbitCoach",
+           "Facebook.InstagramBeta",
+           "Facebook.317180B0BB486",
+           "Expedia.ExpediaHotelsFlightsCarsActivities",
+           "CAF9E577.Plex",
+           "AdobeSystemsIncorporated.PhotoshopElements2018",
+           "A278AB0D.DragonManiaLegends",
+           "A278AB0D.AsphaltStreetStormRacing",
+           "828B5831.TheSecretSociety-HiddenMystery",
+           "USATODAY.USATODAY",
+           "SiliconBendersLLC.Sketchable",
+           "Nordcurrent.CookingFever",
+           "NAVER.LINEwin8",
+           "microsoft.microsoftskydrive",
+           "Microsoft.AgeCastles",
+           "Microsoft.ScreenSketch",
+           "Microsoft.YourPhone",
+           "Microsoft.WebMediaExtensions",
+           "Microsoft.MixedReality.Portal"
+ForEach ($App in $AppList)
+{
+ $PackageFullName = (Get-AppxPackage $App).PackageFullName
+ $ProPackageFullName = (Get-AppxProvisionedPackage -online | where {$_.Displayname -eq $App}).PackageName
+ write-host $PackageFullName
+ Write-Host $ProPackageFullName
+ if ($PackageFullName)
+ 	{
+ 	Write-Host "Removing Package: $App"
+ 	Remove-AppxPackage -package $PackageFullName
+	 }
+ 	else
+	 {
+ 	Write-Host "Unable to find package: $App"
+	 }
+ 	if ($ProPackageFullName)
+ 	{
+	 Write-Host "Removing Provisioned Package: $ProPackageFullName"
+	 Remove-AppxProvisionedPackage -online -packagename $ProPackageFullName
+ 	}
+	 else
+ 	{
+	 Write-Host "Unable to find provisioned package: $App"
+	 }
+ }
