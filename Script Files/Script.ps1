7335f7442e7b3ea6e6d86eefd2e09db5eb9cbcd9
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-04 01:04:15 +0200

    Update Script.ps1

--- Script Files/Script.ps1
+++ Script Files/Script.ps1
@@ -3,11 +3,11 @@
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
 {  
 #Is Powershell 7 Installed
-  $w64=Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
-  $w32=Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"  | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
-if ($w64 -or $w32)
-{
-  Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+	if (Test-Path -Path "$env:ProgramFiles\PowerShell\7\")
+	{
+       Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+
+	}
 # Check if Windows Terminal is Running, Stop Windows Terminal if Running
     if((get-process "WindowsTerminal" -ea SilentlyContinue) -eq $Null){ 
         echo "" 
@@ -62,10 +62,9 @@ Else{
         }
   Break
     }
-}
+
 
 Clear-Host
-#Requires -RunAsAdministrator
 
 #Areyousure function. Alows user to select y or n when asked to exit. Y exits and N returns to main menu.  
  function areyousure {$areyousure = read-host "Are you sure you want to exit? (y/n)"  
