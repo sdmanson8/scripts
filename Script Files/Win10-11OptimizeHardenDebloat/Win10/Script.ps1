2425bd56ef6bac700d5da66f085223e716bd8299
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-03 23:23:11 +0200

    Win10 Optimize, Hardening and Debloating Script

---
+++ Script Files/Win10-11OptimizeHardenDebloat/Win10/Script.ps1
@@ -0,0 +1,144 @@
+$Host.UI.RawUI.WindowTitle = "Main Script for Windows 10 Optimizer"
+
+Write-Host "`nLet's Start with the Basics...`n"
+Start-Sleep -Seconds 1
+
+#Set TimeZone
+Write-Host "`nSet your TimeZone..`n"
+
+$key = Read-Host "Enter the City for your Time Zone WITHOUT "" "" ..."
+Get-TimeZone -ListAvailable | Where-Object {$_.displayname -match "$key"}
+$key2 = Read-Host "Enter the 'Id' for your Time Zone WITHOUT "" "" ..."
+Set-TimeZone -Id "$key2"
+
+#Activate Windows
+Write-Host "`nActivate Windows..`n"
+Start-Sleep -Seconds 1
+$ActivateWindows = Read-Host "Do you want to Activate Windows using OEM Key? [Y = OEM | N = Own Key / Skip License Activation]"
+if ($ActivateWindows -eq 'y') {
+$ProductKey = (Get-CimInstance -ClassName SoftwareLicensingService).OA3xOriginalProductKey
+  if ($null -ne $ProductKey)
+    {
+        start-process c:\Windows\System32\changePK.exe -ArgumentList "/ProductKey $ProductKey"
+    }
+}
+else {
+Write-Host @writecolor "Do you want to use your own Windows Product key? (Y = Yes | N = Skip License Activation)"
+$confirmation = Read-Host 
+if ($confirmation -eq 'y') {
+    Write-Host @writecolor "Please Enter your Genuine 25 Digit Product key"
+    $key = Read-Host 
+    changepk.exe /ProductKey $key
+    Start-Sleep -Seconds 2
+    slmgr.vbs /ato
+    }
+}
+
+Write-Host "`nPreparing to Configure your Computer.. Please Wait`n"
+Start-Sleep -Seconds 1
+
+#Create A Restore Point
+	$SystemDriveUniqueID = (Get-Volume | Where-Object -FilterScript {$_.DriveLetter -eq "$($env:SystemDrive[0])"}).UniqueID#
+	$SystemProtection = ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SPP\Clients")."{09F7EDC5-294E-4180-AF6A-FB0E6A0E9513}") | Where-Object -FilterScript {$_ -match [regex]::Escape($SystemDriveUniqueID)}
+
+    $ComputerRestorePoint = $false
+
+	switch ($null -eq $SystemProtection)
+	{
+		$true
+		{
+			$ComputerRestorePoint = $true
+			Enable-ComputerRestore -Drive $env:SystemDrive
+		}
+	}
+	# Never skip creating a restore point
+	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name SystemRestorePointCreationFrequency -PropertyType DWord -Value 0 -Force
+
+	Checkpoint-Computer -Description "testing" -RestorePointType MODIFY_SETTINGS
+
+	# Revert the System Restore checkpoint creation frequency to 1440 minutes
+	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name SystemRestorePointCreationFrequency -PropertyType DWord -Value 1440 -Force
+
+	# Turn off System Protection for the system drive if it was turned off before without deleting the existing restore points
+	if ($ComputerRestorePoint)
+	{
+		Disable-ComputerRestore -Drive $env:SystemDrive
+	}   
+
+    #Stops edge from taking over as the default .PDF viewer    
+    Write-Host "Stopping Edge from taking over as the default .PDF viewer"
+    # Identify the edge application class 
+    $Packages = "HKCU:SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\Repository\Packages" 
+    $edge = Get-ChildItem $Packages -Recurse -include "MicrosoftEdge" 
+    
+    # Specify the paths to the file and URL associations 
+    $FileAssocKey = Join-Path $edge.PSPath Capabilities\FileAssociations 
+    $URLAssocKey = Join-Path $edge.PSPath Capabilities\URLAssociations 
+    
+    # get the software classes for the file and URL types that Edge will associate 
+    $FileTypes = Get-Item $FileAssocKey 
+    $URLTypes = Get-Item $URLAssocKey 
+    
+    $FileAssoc = Get-ItemProperty $FileAssocKey 
+    $URLAssoc = Get-ItemProperty $URLAssocKey 
+    
+    $Associations = @() 
+    $Filetypes.Property | foreach { $Associations += $FileAssoc.$_ } 
+    $URLTypes.Property | foreach { $Associations += $URLAssoc.$_ }
+
+    # add registry values in each software class to stop edge from associating as the default 
+    foreach ($Association in $Associations) { 
+        $Class = Join-Path HKCU:SOFTWARE\Classes $Association 
+        #if (Test-Path $class) 
+        #   {write-host $Association} 
+        # Get-Item $Class 
+        Set-ItemProperty $Class -Name NoOpenWith -Value "" 
+        Set-ItemProperty $Class -Name NoStaticDefaultVerb -Value "" 
+    } 
+
+#Change Performance Options to Adjust for best performance
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name VisualFXSetting -Type "DWORD" -Value 2 -Force
+
+#Storage Sense
+    #Frequency Run Every 7 Days
+    $7DFrequencyRun = Read-Host -Prompt "Do you want to run Storage Sense every 7 Days? [Y/N]"
+    if ($7DFrequencyRun -eq 'y') {  
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 2048 -PropertyType DWord -Value 7 -Force
+       } else {
+    #Frequency Run Every 30 Days
+    Write-Host "Setting Storage Sense to run every 30 days"
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 2048 -PropertyType DWord -Value 30 -Force
+      }  
+           
+    #Clean-Up Recycle Bin Every 14 Days
+    $7DRecycleBinCleanup = Read-Host -Prompt "Do you want to clean the Recycle Bin every 14 Days? [Y/N]"
+    if ($7DRecycleBinCleanup -eq 'y') { 
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 256 -PropertyType DWord -Value 14 -Force
+      } else {    
+    #Clean-Up Recycle Bin Every 30 Days
+    Write-Host "Setting Cleanup for the Recycle Bin every 30 days"
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 256 -PropertyType DWord -Value 30 -Force
+      }
+
+    #Clean-up Download Folder every 14 days
+    $7DDownloadFolderCleanup = Read-Host -Prompt "Do you want to clean your Downloads Folder every 14 Days? [Y/N]"
+    if ($7DDownloadFolderCleanup -eq 'y') { 
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 512 -PropertyType DWord -Value 14 -Force
+      } else {    
+    #Clean-up Download Folder every 30 days
+    Write-Host "Setting Cleanup for your Downloads Folder every 30 days"
+    New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy -Name 512 -PropertyType DWord -Value 30 -Force
+      }
+
+Powershell.exe "$env:USERPROFILE\Downloads\Win10-11OptimizeHardenDebloat\Win10\Manifest.ps1"
+Write-Warning "Please Restart your Computer !!"
+pwsh.exe "$env:USERPROFILE\Downloads\Win10-11OptimizeHardenDebloat\Win10\Sophia.ps1"
+Write-Warning "Please Restart your Computer !!"
+
+#Reboot Computer
+    # Ask for confirmation to Reboot Computer
+    $Reboot = Read-Host "Would you like to Restart your Computer? (Y/N)"
+    if ($Reboot -eq 'Y') { 
+    Restart-Computer
+}
+
