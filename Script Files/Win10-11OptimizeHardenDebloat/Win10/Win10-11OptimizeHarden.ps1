f4872ca56a006f91f46b4e361f294354ccf76c56
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-05 08:32:57 +0200

    update

--- Script Files/Win10-11OptimizeHardenDebloat/Win10/Win10-11OptimizeHarden.ps1
+++ Script Files/Win10-11OptimizeHardenDebloat/Win10/Win10-11OptimizeHarden.ps1
@@ -50,16 +50,7 @@ Start-Job -Name "Mitigations" -ScriptBlock {
 
     #Enable SEHOP
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" -Name "DisableExceptionChainValidation" -Type "DWORD" -Value 0 -Force
-
-    #Disable NetBIOS by updating Registry
-    #http://blog.dbsnet.fr/disable-netbios-with-powershell#:~:text=Disabling%20NetBIOS%20over%20TCP%2FIP,connection%2C%20then%20set%20NetbiosOptions%20%3D%202
-    $key = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
-    Get-ChildItem $key | ForEach-Object { 
-        Write-Host("Modify $key\$($_.pschildname)")
-        $NetbiosOptions_Value = (Get-ItemProperty "$key\$($_.pschildname)").NetbiosOptions
-        Write-Host("NetbiosOptions updated value is $NetbiosOptions_Value")
-    }
-    
+   
     #Disable WPAD
     #https://adsecurity.org/?p=3299
     New-Item -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\" -Name "Wpad" -Force
