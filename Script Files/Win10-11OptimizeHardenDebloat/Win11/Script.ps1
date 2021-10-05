232b9fa767cefae4cfc9f76ee6143ff1fbaeab84
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-05 13:28:03 -0700

    Update Script.ps1

--- Script Files/Win10-11OptimizeHardenDebloat/Win11/Script.ps1
+++ Script Files/Win10-11OptimizeHardenDebloat/Win11/Script.ps1
@@ -231,14 +231,14 @@ Set-Location "$Destination\Win10-11OptimizeHardenDebloat\Win11"
 Start-Sleep -Seconds 1
 Clear-Host
 Set-Location "$Destination\Win10-11OptimizeHardenDebloat\Win11"
-& '.\Win10-11OptimizeHarden.ps1'
+cmd.exe /k .\windows_hardening.cmd
 Clear-Host
+Set-Location "$Destination\Win10-11OptimizeHardenDebloat\Win11"
+& '.\Win10-11 Tweak.ps1'
 Set-Location "$Destination\Win10-11OptimizeHardenDebloat\Win11"
 & '.\Sophia.ps1'
 Start-Sleep -Seconds 1
 Clear-Host
-Set-Location "$Destination\Win10-11OptimizeHardenDebloat\Win11"
-& '.\Win10-11 Tweak.ps1'
 Write-Warning "Please Restart your Computer !!"
 Start-Sleep -Seconds 1
 
