d75d3bb3bbe7bfc5206446eeb213effc4a016556
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-06 03:17:45 +0200

    Update vpn.ps1

--- Script Files/vpn.ps1
+++ Script Files/vpn.ps1
@@ -1,93 +1,25 @@
-#requires -version 5.1
-#Calling Powershell as Admin and setting Execution Policy to Bypass to avoid Cannot run Scripts error
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
-{  
-#Is Powershell 7 Installed
-  $w64=Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
-  $w32=Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"  | Where-Object { try { $_.DisplayName -match "PowerShell 7-x64" } catch { $false } }
-if ($w64 -or $w32)
-{
-  Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-# Check if Windows Terminal is Running, Stop Windows Terminal if Running
-    if((get-process "WindowsTerminal" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "WindowsTerminal"
-        }
-# Check if CMD is Running, Stop CMD if Running
-    if((get-process "cmd" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "cmd"
-        }
-# Check if Powershell is Running, Stop Powershell if Running
-    if((get-process "powershell" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "powershell"
-        }
-# Check if Powershell 7 is Running, Stop Powershell 7 if Running
-    if((get-process "pwsh" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "pwsh"
-        }
-}
-Else{
-  Start-Process powershell -Verb runAs -ArgumentList ("&'" +$myinvocation.mycommand.definition + "'")
-# Check if Windows Terminal is Running, Stop Windows Terminal if Running
-    if((get-process "WindowsTerminal" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "WindowsTerminal"
-        }
-# Check if CMD is Running, Stop CMD if Running
-    if((get-process "cmd" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "cmd"
-        }
-# Check if Powershell is Running, Stop Powershell if Running
-    if((get-process "powershell" -ea SilentlyContinue) -eq $Null){ 
-        echo "" 
-    }
-    else{ 
-    Stop-Process -processname "powershell"
-        }
-  Break
-    }
-}
-
-Clear-Host
-#Requires -RunAsAdministrator
-
 # Ask for confirmation to Create a VPN Profile
     $CreateVPNProfile = Read-Host "Would you like to Create a VPN Profile? (Y/N)"
     if ($CreateVPNProfile -eq 'Y') { 
 # Edit VPN Name, Address, Username, Password
-        $vpnname = "VPN"
-        $address = "vpn.example.com"
-            Write-Host Add VPN configuration
-            Add-VpnConnection -Name "$vpnname" -ServerAddress "$address" -TunnelType PPTP -EncryptionLevel Required -RememberCredential -PassThru
+        $vpnname = Read-Host "Enter the VPN Name 'Example: VPN' WITHOUT "" "" ..."
+        $address = Read-Host "Enter the VPN Server Address 'Example: vpn.example.com' WITHOUT "" "" ..."
+        Write-Host Add VPN configuration
+        Add-VpnConnection -Name "$vpnname" -ServerAddress "$address" -TunnelType Pptp -EncryptionLevel Required -RememberCredential -PassThru
             }
 
 # Edit VPN Name, Address, Username, Password
-
-        $vpnname = "example VPN"
-        $address = "vpn.example.com"
-        $vpnusername = "password"
-        $vpnpassword = "password"
+        $vpnname = Read-Host ""Enter the VPN Name"" "WITHOUT "" "" ..."
+        $vpnusername = Read-Host "Enter your Domain Username WITHOUT "" "" ..."
+        $vpnpassword = Read-Host "Enter your Domain Password" -AsSecureString;
+        $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($vpnpassword);
+        $password=[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
             Write-Host Connecting to VPN configuration
             $vpn = Get-VpnConnection | where {$_.Name -eq "$vpnname"}
             if ($vpn.ConnectionStatus -eq "Disconnected")
             {
                 $cmd = $env:WINDIR + "\System32\rasdial.exe"
-                $expression = "$cmd ""$vpnname"" $vpnusername $vpnpassword"
+                $expression = "$cmd ""$vpnname"" $vpnusername $password"
                 Invoke-Expression -Command $expression 
             }
+
