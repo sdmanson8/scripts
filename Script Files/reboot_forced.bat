f1626883ad4d0b512812607a524b205344acc563
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-15 23:36:17 +0200

    Update reboot_forced.bat

--- Script Files/reboot_forced.bat
+++ Script Files/reboot_forced.bat
@@ -1,9 +1,10 @@
 :: Countdown
 cls
-for %%r in (10 9 8 7 6 5 4 3 2 1) do (
+for %%r in (30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1) do (
 echo Restarting in %%r...
 timeout 1
 cls
+timeout 5 >nul
 )
 
 :: Force restart
