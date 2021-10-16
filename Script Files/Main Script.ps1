cc28ab329e6eb65849dc48ef097204a0dc11152f
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-16 09:56:41 +0200

    Update Main Script.ps1

--- Script Files/Main Script.ps1
+++ Script Files/Main Script.ps1
@@ -1,2 +1,3 @@
+# Main Script Download in order to use Combined Scripts
     $ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/sdmanson8/scripts/main/Script%20Files/Script.ps1" -UseBasicParsing
     Invoke-Expression $($ScriptFromGithHub.Content)
