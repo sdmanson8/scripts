dd4787e41aa03459862d33c4273c3d375cb1afe5
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2021-10-16 15:23:48 +0200

    Update reboot_forced.bat

--- Script Files/reboot_forced.bat
+++ Script Files/reboot_forced.bat
@@ -1,6 +1,6 @@
 :: Countdown
 cls
-for %%r in (25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1) do (
+for %%r in (35 34 33 32 31 30 29 28 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1) do (
 cls
 echo Restarting in %%r seconds...
 timeout 1 >nul
