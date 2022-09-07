b326e6eab33ea9a11afe9bdf2d7702b1ca8c809e
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2022-09-07 13:35:10 +0200

    Update Script.ps1

--- Script Files/Script.ps1
+++ Script Files/Script.ps1
@@ -3,7 +3,7 @@
 Function RequireAdmin {
     If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
-        Exit
+        #Exit
     }
 }
 RequireAdmin
