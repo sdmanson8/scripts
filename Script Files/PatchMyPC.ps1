8eea5453efc3fb68fb3e6bbd4d7948ba52558e82
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2024-12-24 12:21:31 +0200

    Update PatchMyPC.ps1

--- Script Files/PatchMyPC.ps1
+++ Script Files/PatchMyPC.ps1
@@ -1,2 +1,3 @@
-Invoke-WebRequest -URI https://patchmypc.com/freeupdater/PatchMyPC.exe -OutFile $env:SystemDrive\PatchMyPC.exe -UseBasicParsing
-& $env:SystemDrive\PatchMyPC.exe
+$downloads=(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+Invoke-WebRequest -URI "https://homeupdater.patchmypc.com/public/PatchMyPC-HomeUpdater.msi?_gl=1*1gxmdjs*_gcl_au*MTg3OTQ2MTQ2Ny4xNzM1MDM1MjQ5" -OutFile $downloads\PatchMyPC.msi -UseBasicParsing
+& $downloads\PatchMyPC.msi
